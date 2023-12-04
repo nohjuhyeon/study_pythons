238b703bdb636b10023043a2e7722cb4a1adb8fa
nohjuhyeon <njh2720@gmail.com>
2023-12-04 16:08:22 +0900

    재제출

--- docs/quests/onehandgame.py
+++ docs/quests/onehandgame.py
@@ -3,25 +3,18 @@ str_second = ""
 str_third = "2"
 str_fourth =""
 str_fifth = "3"
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_first = ""
-str_second = "1"
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_third = ""
-str_fourth = "2"
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_third = "3"
-str_fifth = ""
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_first = "3"
-str_third = ""
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_second = ""
-str_third  = "1"
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_third = ""
-str_fifth = "1"
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
-str_fourth =""
-str_third = "2"
-print("{}, {}, {}, {},{}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_first, str_second = str_second, str_first
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_third, str_fourth = str_fourth, str_third
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_third, str_fifth = str_fifth, str_third
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_first, str_third = str_third, str_first
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_second, str_third  = str_third, str_second
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_third, str_fifth = str_fifth, str_third
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
+str_fourth, str_third = str_third, str_fourth
+print(" {}, {}, {}, {}, {}" .format(str_first, str_second, str_third, str_fourth, str_fifth))
