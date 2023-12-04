638a6d2d8b881dc765fcfadd99d693bfece6b098
nohjuhyeon <njh2720@gmail.com>
2023-12-04 15:53:51 +0900

    quest 제출

--- docs/welcomes.py
+++ docs/welcomes.py
@@ -3,6 +3,7 @@
 helloworld = "Hello, World! Juehyeon Noh." # 문자형 변수
 print(helloworld)
 
+print("Hello, World! Juehyeon Noh.") #상수
 # 숫자 합산 출력
 numbers = 3 + 5 # 숫자형 변수
 print(numbers)
@@ -12,5 +13,6 @@ print(numbers)
 
 # 가정문
 if 5 > 2: # 묶음 기호인 :과 tab은 하나의 쌍
+    pass #디버깅할 때 유용
     print("Five is greater than two!")
 print("end")
