5d07673a7c8c03cbd3bb950e5a1bd0e6493e2a05
nohjuhyeon <njh2720@gmail.com>
2023-12-05 12:16:12 +0900

    과제 제출

--- docs/quests/inputwiththree_output.py
+++ docs/quests/inputwiththree_output.py
@@ -1,7 +1,7 @@
 #quest
 #가로  * 세로 * 높이 = 직육면체
 #input: 가로 세로 높이
-#output: 가로(4)m * 세로(1)m * 높이(1)m = 직육면체(4)m
+#output: 가로(4)m * 세로(1)m * 높이(1)m = 직육면체(4)m^3
 
 num_first,num_second,num_third = map(int, input() .split())
 cuboid = num_first * num_second * num_third
