e4fbdc92c34ba2b1c1b17152320d6bcbeaac803e
nohjuhyeon <njh2720@gmail.com>
2023-12-05 15:01:08 +0900

    booleans

--- docs/codingtests/9498.py
+++ docs/codingtests/9498.py
@@ -9,12 +9,14 @@
 
 # 출력
 # 시험 성적을 출력한다.
-score = map(int, input())
+score = int(input())
 if score >= 90 and score <= 100:
     print("A")
 elif score >= 80 and score <= 89:
     print("B")
 elif score >= 70 and score <= 79:
-    print("B")
-elif score >= 80 and score <= 89:
-    print("B")
+    print("C")
+elif score >= 60 and score <= 69:
+    print("D")
+else:
+    print("F")
---
+++ docs/data_types_booleans.py
@@ -0,0 +1,18 @@
+bool_flag = True
+pass
+bool_flag = False
+type(bool_flag)
+# <class 'bool'>
+
+# 문자 판단 
+# 같음에 대한 기호는 ==
+str_name = "juhyeon Noh"
+str_name == "juhyeon Noh"
+# True
+str_name == "juhyeonNOh"
+# False
+# 다름에 대한 기호는 !=
+str_name !="yojulab"
+# True
+str_name != "juhyeon Noh"
+# False
