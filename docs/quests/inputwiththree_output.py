316b304b2aa1eaff42f13fd7e9d315420a1dccbe
nohjuhyeon <njh2720@gmail.com>
2023-12-05 12:24:57 +0900

    과제 재제출

--- docs/quests/inputwiththree_output.py
+++ docs/quests/inputwiththree_output.py
@@ -3,6 +3,9 @@
 #input: 가로 세로 높이
 #output: 가로(4)m * 세로(1)m * 높이(1)m = 직육면체(4)m^3
 
-num_first,num_second,num_third = map(int, input() .split())
-cuboid = num_first * num_second * num_third
-print("가로({})m * 세로({})m * 높이({})m = 직육면체({})m^3" .format(num_first, num_second, num_third, cuboid))
+num_first,num_second,num_third = input().split()
+first = int(num_first)
+second = int(num_second)
+third = int(num_third)
+cuboid = first * second * third
+print("가로({})m * 세로({})m * 높이({})m = 직육면체({})m^3" .format(first, second, third, cuboid))
