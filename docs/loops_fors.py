322ae2b7b4b379ea90a63f4150404bf0544f3120
nohjuhyeon <njh2720@gmail.com>
2023-12-06 11:27:15 +0900

    gugudan

--- docs/loops_fors.py
+++ docs/loops_fors.py
@@ -1,6 +1,28 @@
+# for 기본 문형
+for x in []:
+    pass
+
 # 얼마만큼 반복할지에 대한 값들을 알려줌
 numeric = 0
-numerics = [0, 1, 2, 3, 4] # 5개수
+numerics = [0, 1, 2, 3, 4] # 5개수 값으로 이루어진 리스트
 for number in numerics:
     pass
     print(number)
+
+# 문자 3개 값들로 이루어진 리스트
+for x in ["apple", "banana", "cherry"]:
+    pass
+    print("fruit name : {}!".format(x))
+
+list_fruits = ["apple", "banana", "cherry"]
+for str_fruit in list_fruits:
+    pass
+    print("fruit name : {}!" .format(str_fruit))
+print("End program!")
+
+numerics = [0, 1, 2, 3, 4] # 5개수 값으로 이루어진 리스트
+for number in numerics:
+    pass
+    # print("Number : {}." .format(number))
+    print("Number : {}." .format(number+2))
+print("End program!")
---
+++ docs/quests/timestables_fors.py
@@ -0,0 +1,6 @@
+#구구단 5단 단계별 표시(for문 사용)
+gugudan = int(input("단수:"))
+for x in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    pass
+    print("{} X {} = {}" .format(gugudan,x,x*5))
+    pass
