7f2e734e3458bdff18503f7d1d9f5877cb62556d
nohjuhyeon <njh2720@gmail.com>
2023-12-06 11:28:28 +0900

    gugudan

--- docs/quests/timestables_fors.py
+++ docs/quests/timestables_fors.py
@@ -2,5 +2,5 @@
 gugudan = int(input("단수:"))
 for x in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
     pass
-    print("{} X {} = {}" .format(gugudan,x,x*5))
+    print("{} X {} = {}" .format(gugudan,x,x*gugudan))
     pass
