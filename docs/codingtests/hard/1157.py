4c735b807874e483e2944f6425d95b91d1cf431b
nohjuhyeon <njh2720@gmail.com>
2023-12-11 02:59:44 +0900

    bb

--- docs/codingtests/hard/1157.py
+++ docs/codingtests/hard/1157.py
@@ -76,21 +76,125 @@
 # else:
 #     print("?")
 
-A = input().upper()
-list_A=[]
-list_B=[]
-C = 1
-for i in range(len(A)):
-    list_A.append(A.count(A[i]))
-for i in range(len(list_A)):
-    if list_A[i] == max(list_A):
-        list_B.append(A[i])
-for i in range(len(list_B)):
-    if list_B[i] != list_B[0]:
-        C = 0
+# A = input().upper()
+# list_A=[]
+# list_B=[]
+# C = 1
+# for i in range(len(A)):
+#     list_A.append(A.count(A[i]))
+# for i in range(len(list_A)):
+#     if list_A[i] == max(list_A):
+#         list_B.append(A[i])
+# for i in range(len(list_B)):
+#     if list_B[i] != list_B[0]:
+#         C = 0
+#     else:
+#         pass
+# if C == 1:
+#     print(list_B[0])
+# else:
+#     print("?")
+
+# A = input().upper()
+# for i in range(len(A)):
+#     B = A.count(A[0])
+#     if A.count(A[i]) >= B:
+#         B = A.count(A[i])
+#         C = A[i]
+# for i in range(len(A)):
+#     if A.count(A[i]) >= B:
+#         if A[i] == C:
+#             pass
+#         else:
+#             C = "?"
+# print(C)
+
+# from sys import stdin
+# A = stdin.readline().upper()
+# count_list = [0]*len(A)
+# index_list=[0]*len(count_list)
+# for i in range(len(A)):
+#     B = 0
+#     for j in range(len(A)):
+#         if A[j] == A[i]:
+#             B = B + 1
+#             pass
+#         pass
+#     count_list[i] = B
+#     pass
+# pass
+# max_count_list = count_list[0]
+# for i in range(len(count_list)):
+#     if count_list[i] >= max_count_list:
+#         max_count_list = count_list[i]
+# for i in range(len(count_list)):
+#     if count_list[i] == max_count_list:
+#         index_list[i]= i
+# D = 1
+# for i in range(len(index_list)):
+#     pass
+#     if A[index_list[i]] == A[index_list[0]]:
+#         D = D * 1
+#     else:
+#         D = 0
+# if D == 1:
+#     print(A[index_list[0]])
+# else:
+#     print("?")
+
+# A = input().upper()
+# A_list = []
+# B_list = []
+# C_list = []
+# for i in range(len(A)):
+#     B = 0
+#     for j in range(i,len(A)):
+#         if A[i] != A[j]:
+#             B = B + 1
+#             pass
+#     A_list.append(B)
+# for i in range(len(A_list)):
+#     if A_list[i] == 1:
+#         B_list.append(A[i])
+# for i in range(len(B_list)):
+#     C = A.count(B_list[i])
+#     C_list.append(C)
+# if C_list.count(max(C_list)) > 1:
+#     print("?")
+# else:
+#     D = C_list.index(max(C_list))
+#     print(A[D])
+
+# A = list(input().upper())
+# list_B = []
+# for i in range(len(A)):
+#     B = 0
+#     for j in range(i,len(A)):
+#         if A[i] == A[j]:
+#             B = B + 1
+#     list_B.append(B)
+# max = list_B[0]
+# for i in range(len(list_B)):
+#     if list_B[i] >= max:
+#         max = list_B[i]
+# if list_B.count(max) > 1:
+#     print("?")
+# else:
+#     print(A[list_B.index(max)])
+
+A = list(input().upper())
+B = list(set(A))
+C = []
+for i in range(len(B)):
+    C.append(A.count(B[i]))
+max = C[0]
+for i in range(len(C)):
+    if C[i] >= max:
+        max = C[i]
+        pass
     else:
         pass
-if C == 1:
-    print(list_B[0])
-else:
+if C.count(max) > 1:
     print("?")
+else:
+    print(B[C.index(max)])
