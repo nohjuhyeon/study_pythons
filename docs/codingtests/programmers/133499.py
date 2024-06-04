04c3509799c08de7527a3380ffc673b86bf3cccf
nohjuhyeon <njh2720@gmail.com>
2024-06-04 14:59:15 +0900

    codingtest

--- docs/codingtests/programmers/133499.py
+++ docs/codingtests/programmers/133499.py
@@ -25,9 +25,20 @@
 def solution(babbling):
     list_babbling = ["aya", "ye", "woo", "ma"]
     for i in range(len(babbling)):
-        for j in range(len(list_babbling)):
-            if list_babbling[j] in babbling[i]:
-                babbling[i]=babbling[i].replace(list_babbling[j]," ")
+        str_answer = ''
+        last_babbling = ''
+        while True:
+            babbling_str = babbling[i]
+            for j in range(len(list_babbling)):
+                if babbling[i].startswith(list_babbling[j]) and last_babbling != list_babbling[j]:
+                    babbling[i] = babbling[i][len(list_babbling[j]):]
+                    str_answer = str_answer+list_babbling[j]
+                    last_babbling = list_babbling[j]
+                    break
+            if babbling_str == babbling[i]:
+                break
+            else:
+                babbling_str = babbling[i]
     for i in range(len(babbling)):
         babbling[i] = babbling[i].replace(" ","")
     answer = babbling.count("")
